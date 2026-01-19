da58ad9787bfd277cf874782afbe1027c8b19a39
Bryan Romero <bryan.romerodev@gmail.com>
2026-01-19 16:44:39 +0100

    Add CORS configuration to allow frontend access
    - Implement `WebConfig` with CORS settings to enable requests from the frontend hosted at `activity-aggregation-fe.vercel.app`.

---
+++ activity-aggregation-be/src/main/java/com/bwromero/activity/aggregation/api/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.bwromero.activity.aggregation.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://activity-aggregation-fe.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
